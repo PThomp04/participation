6884f75fe20c349ee6a0ebd865ea1e5f03d971cd
Parker Thompson <Parker.B.Thompson-1@ou.edu>
2019-09-04 11:16:48 -0500

    participation

---
+++ Participation-9-4-19/Participation-9-4-19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation_9_4_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            DateTime future = today.AddDays(27);
+            
+            Console.WriteLine(today);
+            Console.WriteLine(future);
+
+            var difference = future - today;
+            Console.WriteLine(difference);
+
+
+            Console.ReadKey();
+        }
+    }
+}
